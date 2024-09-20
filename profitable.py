06d22fecf1e1b01fb70375d09bc71ead53b75d41
Tom Manning <tom@manningetal.com>
2024-09-20 10:25:16 -0400

    add a class to avoid having globals

--- profitable.py
+++ profitable.py
@@ -7,159 +7,218 @@ Date format: YYYY-MM-DD
 '''
 
 import sys
-import os
+#import os
 
 import csv
 import datetime
 
-# export TK_SILENCE_DEPRECATION=1
+import argparse
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-# print(filename)
-
-row_number = 0
-PROFIT_THRESHOLD= round(3.5*(20+16)*1.3)  #per day; 2 days at $21, 1 day at $18 in 2024; the 1.3 multiplier is for taxes
-days_tour_count_list_date_key = [dict() for x in range(7)]
-days_tour_count_list_doys_key = [dict() for x in range(7)] #day of year scaled
-start_day_of_year= 365
-end_day_of_year= 0
-start_week_of_year= 52
-end_week_of_year= 0
-graphed_year = None
-tours_total = 0
-tours_per_day_maximum = 0
-revenue_total = 0
-
-csvReader = csv.DictReader(open(filename))
-for row in csvReader:
-   row_number += 1
-   if row['Category'] == 'Tours':
-      tour_date = datetime.datetime.strptime(row['Date'], '%Y-%m-%d') #tour_date includes HH:MM:SS
-      date_w_year= tour_date.strftime('%Y-%m-%d')
-      graphed_year= tour_date.strftime('%Y')
-      # date_wo_year= tour_date.strftime('%m-%d')
-
-      day_of_week= tour_date.weekday()
-      week_of_year= tour_date.isocalendar().week
-      if week_of_year < start_week_of_year:
-         start_week_of_year= week_of_year
-      if week_of_year > end_week_of_year:
-         end_week_of_year= week_of_year
-      day_of_year = tour_date.timetuple().tm_yday
-      if day_of_year < start_day_of_year:
-         start_day_of_year= day_of_year
-         if day_of_week == 5:
-            start_day_of_year -= 1
-         if day_of_week == 6:
-            start_day_of_year -= 2
-      if day_of_year > end_day_of_year:
-         end_day_of_year= day_of_year
-
-      row_tour_count= round(float(row['Qty']))
-      tours_total += row_tour_count
-      revenue_total += int(float(row['Gross Sales'].replace('$', '')))
-
-      if date_w_year in days_tour_count_list_date_key[day_of_week]:
-         days_tour_count_list_date_key[day_of_week][date_w_year] += row_tour_count
-      else:
-         days_tour_count_list_date_key[day_of_week][date_w_year] = row_tour_count
-
-      # days_tour_count_list_doy_key[day_of_week][day_of_year] = days_tour_count_list_date_key[day_of_week][date_w_year]
-   # else:
-   #    print(f"Skipping row={row_number} with category={row['Category']}")
-
-# make another list using scaled day of year for key instead of year-day-month
-for day_of_week in range(4, 7):
-   for date, tour_count in days_tour_count_list_date_key[day_of_week].items():
-      tour_date = datetime.datetime.strptime(date, '%Y-%m-%d')
-      week_of_year= tour_date.isocalendar().week
-      day_of_week= tour_date.weekday()
-      scaled_doy= ((week_of_year - start_week_of_year) * 4) + (day_of_week - 4) #the 4 multiplier leaves a gap between weeks
-      days_tour_count_list_doys_key[day_of_week][scaled_doy] = tour_count
-      if tour_count > tours_per_day_maximum:
-         tours_per_day_maximum= tour_count
+class Parsed_results:
+   def __init__(self):
+      # profit_thold calc: per day; 2 days at $21, 1 day at $18 in 2024; the 1.3 multiplier is for taxes
+      self.PROFIT_THRESHOLD= round(3.5*(20+16)*1.3)
+      self.row_number = 0
+      self.days_tour_count_list_date_key = [dict() for x in range(7)]
+      self.days_tour_count_list_doys_key = [dict() for x in range(7)] #day of year scaled
+      self.START= 0
+      self.END= 1
+      self.day_of_year= [365,0] #START, END
+      self.week_of_year= [52,0] #START, END
+      self.graphed_year = None
+      self.tours_total = 0
+      self.tours_per_day_maximum = 0
+      self.revenue_total = 0
+      self.paid_weeks= 0
+      self.expense= 0
 
 
-print_it = False
-if print_it:
-   print(f"processed {row_number} rows;  starting day of year = {start_day_of_year} starting week of year = {start_week_of_year}")
-   day_label_list= ["Fri", "Sat", "Sun"]
-   for day_of_week in range(4, 7):
-      print(f"{day_label_list[day_of_week-4]}: {days_tour_count_list_date_key[day_of_week]}")
-      # print(f"{day_label_list[day_of_week-4]}: {days_tour_count_list_doy_key[day_of_week]}")
-      print(f"{day_label_list[day_of_week-4]} length={len(days_tour_count_list_date_key[day_of_week])}: {days_tour_count_list_doys_key[day_of_week]}")
+   def parse_csv(self, filename):
+      csvReader = csv.DictReader(open(filename))
+      for row in csvReader:
+         self.row_number += 1
+         if row['Category'] == 'Tours':
+            tour_date = datetime.datetime.strptime(row['Date'], '%Y-%m-%d') #tour_date includes HH:MM:SS
+            date_w_year= tour_date.strftime('%Y-%m-%d')
+            self.graphed_year= tour_date.strftime('%Y')
+            # date_wo_year= tour_date.strftime('%m-%d')
+
+            day_of_week= tour_date.weekday()
+            week_of_year= tour_date.isocalendar().week
+            day_of_year = tour_date.timetuple().tm_yday
+
+            if week_of_year < self.week_of_year[self.START]:
+               self.week_of_year[self.START]= week_of_year
+            if week_of_year > self.week_of_year[self.END]:
+               self.week_of_year[self.END]= week_of_year
+
+            if day_of_year < self.day_of_year[self.START]:
+               self.day_of_year[self.START]= day_of_year
+               # adjust start_day if there were no sales on Friday or Saturday
+               if day_of_week == 5:
+                  self.day_of_year[self.START] -= 1
+               if day_of_week == 6:
+                  self.day_of_year[self.START] -= 2
+            if day_of_year > self.day_of_year[self.END]:
+               self.day_of_year[self.END]= day_of_year
+
+            row_tour_count= round(float(row['Qty']))
+            self.tours_total += row_tour_count
+            self.revenue_total += int(float(row['Gross Sales'].replace('$', '')))
+
+            if date_w_year in self.days_tour_count_list_date_key[day_of_week]:
+               self.days_tour_count_list_date_key[day_of_week][date_w_year] += row_tour_count
+            else:
+               self.days_tour_count_list_date_key[day_of_week][date_w_year] = row_tour_count
+
+            # days_tour_count_list_doy_key[day_of_week][day_of_year] = days_tour_count_list_date_key[day_of_week][date_w_year]
+         # else:
+         #    print(f"Skipping row={row_number} with category={row['Category']}")
+
+      # make another list using scaled day of year for key instead of year-day-month
+      printed_once = True
+      for day_of_week in range(4, 7):
+         for date, tour_count in self.days_tour_count_list_date_key[day_of_week].items():
+            tour_date = datetime.datetime.strptime(date, '%Y-%m-%d')
+            week_of_year= tour_date.isocalendar().week
+            day_of_week= tour_date.weekday()
+            scaled_doy= ((week_of_year - self.week_of_year[self.START]) * 4) + (day_of_week - 4) #the 4 multiplier leaves a gap between weeks
+            self.days_tour_count_list_doys_key[day_of_week][scaled_doy] = tour_count
+            if tour_count > self.tours_per_day_maximum:
+               self.tours_per_day_maximum= tour_count
+            if not printed_once:
+               print(f"{self.week_of_year[self.START]= } {tour_date= } {week_of_year= } {day_of_week= } {scaled_doy= } ")
+               printed_once= True
+
+      self.paid_weeks= (self.week_of_year[self.END]-self.week_of_year[self.START]) + 1 # undetermined why off by 1
+      self.expense= self.paid_weeks*self.PROFIT_THRESHOLD*3
 
 
-paid_weeks= (end_week_of_year-start_week_of_year) + 1 # undetermined why off by 1
-expense= paid_weeks*PROFIT_THRESHOLD*3
-
-analyze_it = True
-if analyze_it:
-   profitable_days_count= [0]*7
-   for day_of_week in range(4, 7):
-      for date, tour_count in days_tour_count_list_date_key[day_of_week].items():
-         if (tour_count * 10) > PROFIT_THRESHOLD:
-            profitable_days_count[day_of_week] += 1
-            
-      print(f"day={day_of_week} "
-            f"tours={sum(days_tour_count_list_date_key[day_of_week].values())} "
-            f"days={len(days_tour_count_list_date_key[day_of_week])} "
-            f"profitable_days={profitable_days_count[day_of_week]}")
-   print(f"{PROFIT_THRESHOLD=} {tours_per_day_maximum=} {paid_weeks= } {expense= }")
+   def print_parse_info(self):
+      print(f"processed {self.row_number} rows;  starting day of year = {self.day_of_year[self.START]} starting week of year = {self.week_of_year[self.START]}")
+      day_label_list= ["Fri", "Sat", "Sun"]
+      for day_of_week in range(4, 7):
+         print(f"{day_label_list[day_of_week-4]}")
+         print(f"\t{day_label_list[day_of_week-4]}: {self.days_tour_count_list_date_key[day_of_week]}")
+         # print(f"{day_label_list[day_of_week-4]}: {days_tour_count_list_doy_key[day_of_week]}")
+         print(f"\t{day_label_list[day_of_week-4]} length={len(self.days_tour_count_list_date_key[day_of_week])}: {self.days_tour_count_list_doys_key[day_of_week]}")
 
 
-plot_it = True
-if plot_it:
-   import matplotlib.pyplot as plt
-   import math
+   def print_analysis_info(self):
+      profitable_days_count= [0]*7
+      for day_of_week in range(4, 7):
+         for date, tour_count in self.days_tour_count_list_date_key[day_of_week].items():
+            if (tour_count * 10) > self.PROFIT_THRESHOLD:
+               profitable_days_count[day_of_week] += 1
+               
+         print(f"day={day_of_week} "
+               f"tours={sum(self.days_tour_count_list_date_key[day_of_week].values())} "
+               f"days={len(self.days_tour_count_list_date_key[day_of_week])} "
+               f"profitable_days={profitable_days_count[day_of_week]}")
+      print(f"{self.PROFIT_THRESHOLD=} {self.tours_per_day_maximum=} {self.paid_weeks= } {self.expense= }")
 
-   background_color = 'black'
-   font_color = 'white'
-   
-   width = 16
-   height = 6
-   fig = plt.figure(figsize = (width, height), facecolor=background_color) #size in inches
-   ax = plt.axes()
-   ax.set_facecolor(background_color) #plot background color
-   ax.spines['bottom'].set_color(font_color)
-   ax.tick_params(axis='x', colors=font_color)
-   ax.spines['left'].set_color(font_color)
-   ax.tick_params(axis='y', colors=font_color)
 
-   revenue = revenue_total
+   def plot_it(self):
+      import matplotlib.pyplot as plt
+      import math
 
-   # text(x, y, string, kwarg**) 
-   ax.text((width/2), tours_per_day_maximum-3, f"Total Tours (not including groups)={tours_total}\nRevenue=${revenue_total}\nExpense=${expense} (Approximate)\n      Loss=${revenue-expense}", 
-         bbox={'facecolor': font_color, 'alpha': 0.8, 'pad': 8})
+      background_color = 'black'
+      font_color = 'white'
+      
+      width = 16
+      height = 6
+      fig = plt.figure(figsize = (width, height), facecolor=background_color) #size in inches
+      ax = plt.axes()
+      ax.set_facecolor(background_color) #plot background color
+      ax.spines['bottom'].set_color(font_color)
+      ax.tick_params(axis='x', colors=font_color)
+      ax.spines['left'].set_color(font_color)
+      ax.tick_params(axis='y', colors=font_color)
 
-   for day_of_week in range(4, 7):
-      plt.bar(list(days_tour_count_list_doys_key[day_of_week].keys()), list(days_tour_count_list_doys_key[day_of_week].values()))
-   
-   plt.axhline(9,color='red') #horizontal profitability line
+      loss= self.revenue_total - self.expense
+      # text(x, y, string, kwarg**) 
+      ax.text((width/2), self.tours_per_day_maximum-3, 
+               f"Total Tours (not including groups)={self.tours_total}\nRevenue=${self.revenue_total}\nExpense=${self.expense} (Approximate)\n      Loss=${loss}", 
+               bbox={'facecolor': font_color, 'alpha': 0.8, 'pad': 8})
 
-   y_tick_spacing = 2
-   plt.yticks(range(0,tours_per_day_maximum + y_tick_spacing, y_tick_spacing))
+      for day_of_week in range(4, 7):
+         plt.bar(list(self.days_tour_count_list_doys_key[day_of_week].keys()), list(self.days_tour_count_list_doys_key[day_of_week].values()))
+      
+      plt.axhline(9,color='red') #horizontal profitability line
 
-   weekend_count = math.ceil((end_day_of_year - start_day_of_year)/7)
-   BARS_PER_WEEKEND = 4
-   plt.xticks(range(1, weekend_count*BARS_PER_WEEKEND, BARS_PER_WEEKEND))
-   labels = [item.get_text() for item in ax.get_xticklabels()]
-   for index, value in enumerate(labels):
-      labels[index] = datetime.datetime.strptime(graphed_year + "-" + str(start_day_of_year + 1 + (index*7)), "%Y-%j").strftime("%b%d")
-   # print(f"labels={labels}")
-   ax.set_xticklabels(labels)
+      y_tick_spacing = 2
+      plt.yticks(range(0,self.tours_per_day_maximum + y_tick_spacing, y_tick_spacing))
 
-   USE_XLABEL = False
-   if USE_XLABEL:
-      start_date = datetime.datetime.strptime(graphed_year + "-" + str(start_day_of_year), "%Y-%j").strftime("%B%d")
-      end_date = datetime.datetime.strptime(graphed_year + "-" + str(end_day_of_year), "%Y-%j").strftime("%B%d")
-      print(f"end_day_of_year={end_day_of_year} ({end_date}); start_day_of_year{start_day_of_year} ({start_date}); diff={end_day_of_year - start_day_of_year}")
-      plt.xlabel(f"{start_date}  -through-  {end_date}", color=font_color)
+      weekend_count = math.ceil((self.day_of_year[self.END] - self.day_of_year[self.START])/7)
+      BARS_PER_WEEKEND = 4
+      plt.xticks(range(1, weekend_count*BARS_PER_WEEKEND, BARS_PER_WEEKEND))
+      labels = [item.get_text() for item in ax.get_xticklabels()]
+      for index, value in enumerate(labels):
+         labels[index] = datetime.datetime.strptime(self.graphed_year + "-" + str(self.day_of_year[self.START] + 1 + (index*7)), "%Y-%j").strftime("%b%d")
+      # print(f"labels={labels}")
+      ax.set_xticklabels(labels)
 
-   plt.ylabel("Tours", color=font_color)
-   plt.title(f"Tours per day for {graphed_year}", color=font_color)
-   plt.legend(['Profitability','Friday', 'Saturday', 'Sunday'])
-   plt.show()
+      USE_XLABEL = False
+      if USE_XLABEL:
+         start_date = datetime.datetime.strptime(self.graphed_year + "-" + str(self.day_of_year[self.START]), "%Y-%j").strftime("%B%d")
+         end_date = datetime.datetime.strptime(self.graphed_year + "-" + str(self.day_of_year[self.END]), "%Y-%j").strftime("%B%d")
+         print(f"end_day_of_year={self.day_of_year[self.END]} ({end_date}); \
+               start_day_of_year{self.day_of_year[self.START]} ({start_date}); \
+               diff={self.day_of_year[self.END] - self.day_of_year[self.START]}")
+         plt.xlabel(f"{start_date}  -through-  {end_date}", color=font_color)
+
+      plt.ylabel("Tours", color=font_color)
+      plt.title(f"Tours per day for {self.graphed_year}", color=font_color)
+      plt.legend(['Profitability','Friday', 'Saturday', 'Sunday'])
+      plt.show()
+
+def pick_file():
+   from tkinter import Tk
+   from tkinter.filedialog import askopenfilename
+
+   # root = Tk.Tk()
+   Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+   file_path = askopenfilename(title="Select an items...csv File")
+    
+   if file_path:
+      filename = file_path.split('/')[-1]  # For Unix/Linux
+      # filename = file_path.split('\\')[-1]  # For Windows
+      # print(f"Selected filename: {filename}")
+      return filename
+   else:
+      print("No file selected.")
+      return None
+
+
+if __name__ == "__main__":
+   argParser = argparse.ArgumentParser()
+   argParser.add_argument("input", nargs='?', type=str, help="input CSV filename with path")
+   argParser.add_argument("-p", "--print_parsed_data", action='store_true', help="if true, print parsed info & dont plot")
+   argParser.add_argument("-a", "--analyze_info_output", action='store_false', help="if true, print analysis info")
+
+   args = argParser.parse_args()
+   # print(f'{args.input= } {args.parse_info_output= } {args.analyze_info_output= }')
+
+   if args.input is None:
+      csv_filename = pick_file()
+      if csv_filename is None:
+         sys.exit("No PDF files selected to parse.")
+      csv_filename = f'../{csv_filename}'
+   else:
+      csv_filename = args.input
+
+   results= Parsed_results()
+   results.parse_csv(csv_filename)
+
+   if args.print_parsed_data:
+      results.print_parse_info()
+
+   if args.analyze_info_output:
+      results.print_analysis_info()
+
+   if args.print_parsed_data:
+      sys.exit(0)
+
+   results.plot_it()
